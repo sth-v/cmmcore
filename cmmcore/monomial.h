02d2b4fee779706a4e98182645fc95b837966b9e
sth-v <sthv.developer@gmail.com>
2024-10-10 04:28:56 +0300

    GaussMap correction

--- cmmcore/monomial.h
+++ cmmcore/monomial.h
@@ -14,7 +14,6 @@
 #endif
 
 namespace cmmcore {
-
     enum SurfaceParameter {
         U,
         V
@@ -30,71 +29,64 @@ namespace cmmcore {
         }
         return result;
     }
+
     template<typename T>
-    using Tensor2D=std::vector<std::vector<T>>;
+    using Tensor2D = std::vector<std::vector<T> >;
 #define CMMCORE_CREATE_TENSOR2D(n,m,typ,...) std::vector(n, std::vector<typ>(m, {__VA_ARGS__}))
 #define CMMCORE_RESIZE_TENSOR2D(var,n,m,typ,...) var.resize(n, std::vector<typ>(m, {__VA_ARGS__}))
-    template<typename T>
-inline void getVectorTensor2DComponent(Tensor2D<T>& arr,  int component,Matrix& result) {
-    result.rows=arr.size();
-    result.cols=arr[0].size();
-    result.data.resize(result.cols*result.rows);
-    for (int i = 0; i < arr.size(); ++i) {
-        for (int j = 0; j < arr[i].size(); ++j) {
-            result(i,j)=arr[i][j][component];
-        }
-    }
-}
-    template<typename T>
-    inline void setVectorTensor2DComponent(Tensor2D<T>& arr, int component,const Matrix& value) {
-    for (int i = 0; i < arr.size(); ++i) {
-        for (int j = 0; j < arr[i].size(); ++j) {
-            arr[i][j][component]=value(i,j);
 
+    template<typename T>
+    inline void getVectorTensor2DComponent(Tensor2D<T> &arr, int component, Matrix &result) {
+        result.rows = arr.size();
+        result.cols = arr[0].size();
+        result.data.resize(result.cols * result.rows);
+        for (int i = 0; i < arr.size(); ++i) {
+            for (int j = 0; j < arr[i].size(); ++j) {
+                result(i, j) = arr[i][j][component];
+            }
         }
     }
-}
+
+    template<typename T>
+    inline void setVectorTensor2DComponent(Tensor2D<T> &arr, int component, const Matrix &value) {
+        for (int i = 0; i < arr.size(); ++i) {
+            for (int j = 0; j < arr[i].size(); ++j) {
+                arr[i][j][component] = value(i, j);
+            }
+        }
+    }
+
     inline void compute_partial_derivative(
-    const Tensor2D<vec3> & coeffs,
-    const SurfaceParameter variable,
-    Tensor2D<vec3>& result ) {
+        const Tensor2D<vec3> &coeffs,
+        const SurfaceParameter variable,
+        Tensor2D<vec3> &result) {
         int n = coeffs.size();
         int m = coeffs[0].size();
 
 
-
-
         if (variable == U) {
-            CMMCORE_RESIZE_TENSOR2D(result,n-1,m,vec3,0,0,0);
+            CMMCORE_RESIZE_TENSOR2D(result, n-1, m, vec3, 0, 0, 0);
             for (int i = 1; i < n; ++i) {
                 for (int j = 0; j < m; ++j) {
-
-                        result[i - 1][j][0] = i * coeffs[i][j][0];
-                        result[i - 1][j][1] = i * coeffs[i][j][1];
-                        result[i - 1][j][2] = i * coeffs[i][j][2];
-
+                    result[i - 1][j][0] = i * coeffs[i][j][0];
+                    result[i - 1][j][1] = i * coeffs[i][j][1];
+                    result[i - 1][j][2] = i * coeffs[i][j][2];
                 }
             }
         } else if (variable == V) {
-            CMMCORE_RESIZE_TENSOR2D(result,n,m-1,vec3,0,0,0);
+            CMMCORE_RESIZE_TENSOR2D(result, n, m-1, vec3, 0, 0, 0);
 
-            for (int i = 0; i < n; ++i){
-            for (int j = 1; j < m; ++j) {
-
-
-                        result[i][j - 1][0] = j * coeffs[i][j][0];
-                        result[i][j - 1][1] = j * coeffs[i][j][1];
-                        result[i][j - 1][2] = j * coeffs[i][j][2];
-
-
+            for (int i = 0; i < n; ++i) {
+                for (int j = 1; j < m; ++j) {
+                    result[i][j - 1][0] = j * coeffs[i][j][0];
+                    result[i][j - 1][1] = j * coeffs[i][j][1];
+                    result[i][j - 1][2] = j * coeffs[i][j][2];
                 }
             }
         }
-
-
     }
 
-    inline void cross(const Tensor2D<vec3> & a_coeffs, const Tensor2D<vec3> & b_coeffs, Tensor2D<vec3> & result) {
+    inline void cross(const Tensor2D<vec3> &a_coeffs, const Tensor2D<vec3> &b_coeffs, Tensor2D<vec3> &result) {
         int n1 = a_coeffs.size();
         int m1 = a_coeffs[0].size();
         int n2 = b_coeffs.size();
@@ -103,7 +95,7 @@ inline void getVectorTensor2DComponent(Tensor2D<T>& arr,  int component,Matrix& 
         int m = m1 + m2 - 1;
 
         // Initialize the result array with zeros
-        CMMCORE_RESIZE_TENSOR2D(result,n,m,vec3,0,0,0);
+        CMMCORE_RESIZE_TENSOR2D(result, n, m, vec3, 0, 0, 0);
 
 
         // Compute the cross product
@@ -113,57 +105,75 @@ inline void getVectorTensor2DComponent(Tensor2D<T>& arr,  int component,Matrix& 
                     for (int j2 = 0; j2 < m2; ++j2) {
                         int i = i1 + i2;
                         int j = j1 + j2;
-                        result[i][j][0] += (a_coeffs[i1][j1][1] * b_coeffs[i2][j2][2] - a_coeffs[i1][j1][2] * b_coeffs[i2][j2][1]);
-                        result[i][j][1] += (a_coeffs[i1][j1][2] * b_coeffs[i2][j2][0] - a_coeffs[i1][j1][0] * b_coeffs[i2][j2][2]);
-                        result[i][j][2] += (a_coeffs[i1][j1][0] * b_coeffs[i2][j2][1] - a_coeffs[i1][j1][1] * b_coeffs[i2][j2][0]);
+                        result[i][j][0] += (a_coeffs[i1][j1][1] * b_coeffs[i2][j2][2] - a_coeffs[i1][j1][2] * b_coeffs[
+                                                i2][j2][1]);
+                        result[i][j][1] += (a_coeffs[i1][j1][2] * b_coeffs[i2][j2][0] - a_coeffs[i1][j1][0] * b_coeffs[
+                                                i2][j2][2]);
+                        result[i][j][2] += (a_coeffs[i1][j1][0] * b_coeffs[i2][j2][1] - a_coeffs[i1][j1][1] * b_coeffs[
+                                                i2][j2][0]);
                     }
                 }
             }
         }
-
-
     }
 
     class Monomial2D {
     public:
-        int n,m;
+        int n, m;
         Tensor2D<vec3> coefficients;
-        Matrix Mu,Mv,Mu_inv,Mv_inv;
-        Monomial2D()=default;
-        Monomial2D(int _n, int _m, const Tensor2D<vec3>& coeffs): n(_n),
-                                                            m(_m),
-                                                            coefficients(coeffs),
-                                                            Mu(bpmat(n - 1)),
-                                                            Mv(bpmat(m - 1)),
-    Mu_inv(std::move(Mu.inverse())),
-                                                    Mv_inv(std::move(Mv.inverse())){}
+        Matrix Mu, Mv, Mu_inv, Mv_inv;
+
+        Monomial2D() = default;
+
+        Monomial2D(int _n, int _m, const Tensor2D<vec3> &coeffs): n(_n),
+                                                                  m(_m),
+                                                                  coefficients(coeffs),
+                                                                  Mu(bpmat(n - 1)),
+                                                                  Mv(bpmat(m - 1)),
+                                                                  Mu_inv(std::move(Mu.inverse())),
+                                                                  Mv_inv(std::move(Mv.inverse())) {
+        }
 
 
+        Monomial2D(int _n, int _m): n(_n), m(_m), coefficients(CMMCORE_CREATE_TENSOR2D(_n, _m, vec3, 0, 0, 0)),
+                                    Mu(bpmat(n - 1)), Mv(bpmat(m - 1)), Mu_inv(std::move(Mu.inverse())),
+                                    Mv_inv(std::move(Mv.inverse())) {
+        }
 
+        Monomial2D(Tensor2D<vec3> &coeffs): n(coeffs.size()), m(coeffs[0].size()), coefficients(std::move(coeffs)),
+                                            Mu(bpmat(n - 1)), Mv(bpmat(m - 1)), Mu_inv(std::move(Mu.inverse())),
+                                            Mv_inv(std::move(Mv.inverse())) {
+        }
 
-    Monomial2D(int _n, int _m): n(_n), m(_m), coefficients(CMMCORE_CREATE_TENSOR2D(_n,_m,vec3,0,0,0)),Mu(bpmat(n - 1)),Mv(bpmat(m - 1)),Mu_inv(std::move(Mu.inverse())),Mv_inv(std::move(Mv.inverse())){}
-    Monomial2D( Tensor2D<vec3>& coeffs): n(coeffs.size()),m(coeffs[0].size()),coefficients(std::move(coeffs)),Mu(bpmat(n - 1)),Mv(bpmat(m - 1)),Mu_inv(std::move(Mu.inverse())),Mv_inv(std::move(Mv.inverse())){}
-    Monomial2D(const Monomial2D& m):n(m.n),m(m.m),coefficients(m.coefficients),Mu(m.Mu),Mv(m.Mv),Mu_inv(m.Mu_inv),Mv_inv(m.Mv_inv) {}
-    Monomial2D(const NURBSSurface& surf): n(surf._size[0]),m(surf._size[1]),coefficients(CMMCORE_CREATE_TENSOR2D(n,m,vec3,0,0,0)),Mu(bpmat(n - 1)),Mv(bpmat(m - 1)),Mu_inv(std::move(Mu.inverse())),Mv_inv(std::move(Mv.inverse())) {
+        Monomial2D(const Monomial2D &m): n(m.n), m(m.m), coefficients(m.coefficients), Mu(m.Mu), Mv(m.Mv),
+                                         Mu_inv(m.Mu_inv), Mv_inv(m.Mv_inv) {
+        }
+
+        Monomial2D(const NURBSSurface &surf): n(surf._size[0]), m(surf._size[1]),
+                                              coefficients(CMMCORE_CREATE_TENSOR2D(n, m, vec3, 0, 0, 0)),
+                                              Mu(bpmat(n - 1)), Mv(bpmat(m - 1)), Mu_inv(std::move(Mu.inverse())),
+                                              Mv_inv(std::move(Mv.inverse())) {
             Tensor2D<vec3> cpts = surf.control_points3d();
             Matrix MvT = Mv.transpose();
             for (int i = 0; i < 3; ++i) {
                 Matrix cfs(n, m);
                 getVectorTensor2DComponent<vec3>(cpts, i, cfs);
-                setVectorTensor2DComponent<vec3>(coefficients, i, Mu * cfs * MvT); // Mu @ control_points[:, :, d] @ Mv.T
+                setVectorTensor2DComponent<vec3>(coefficients, i, Mu * cfs * MvT);
+                // Mu @ control_points[:, :, d] @ Mv.T
             }
         }
-        void set(Tensor2D<vec3>& coeffs) {
-            n=coeffs.size();
-            m=coeffs[0].size();
-            coefficients=std::move(coeffs);
-            Mu=bpmat(n - 1);
-            Mv=bpmat(m - 1);
-            Mu_inv=std::move(Mu.inverse());
-            Mv_inv=std::move(Mv.inverse());
 
+        void set(Tensor2D<vec3> &coeffs) {
+            n = coeffs.size();
+            m = coeffs[0].size();
+            coefficients = std::move(coeffs);
+            Mu = bpmat(n - 1);
+            Mv = bpmat(m - 1);
+            Mu_inv = std::move(Mu.inverse());
+            Mv_inv = std::move(Mv.inverse());
         }
-        void to_bezier(NURBSSurface& surf) {
+
+        void to_bezier(NURBSSurface &surf) {
             /*
             *    n, m, dim = monomial_coeffs.shape
             #print(n - 1)
@@ -173,94 +183,87 @@ inline void getVectorTensor2DComponent(Tensor2D<T>& arr,  int component,Matrix& 
 
             control_points = np.zeros((n, m, dim))
             for d in range(dim):
-            control_points[:, :, d] = Mu_inv @ monomial_coeffs[:, :, d] @ Mv_inv.T
-*/
-            CMMCORE_RESIZE_TENSOR2D(surf._control_points,n,m,vec4,0,0,0,1);
-            Matrix Mv_inv_T=Mv_inv.transpose();
+            control_points[:, :, d] = Mu_inv @ monomial_coeffs[:, :, d] @ Mv_inv.T*/
+            CMMCORE_RESIZE_TENSOR2D(surf._control_points, n, m, vec4, 0, 0, 0, 1);
+            Matrix Mv_inv_T = Mv_inv.transpose();
             for (int i = 0; i < 3; ++i) {
                 Matrix cfs(n, m);
                 getVectorTensor2DComponent<vec3>(coefficients, i, cfs);
-                setVectorTensor2DComponent<vec4>(surf._control_points,i,Mu_inv*cfs*Mv_inv_T);
+                setVectorTensor2DComponent<vec4>(surf._control_points, i, Mu_inv * cfs * Mv_inv_T);
             }
-            surf._degree[0]=n-1;
-            surf._degree[1]=m-1;
-            surf._size[0]=n;
-            surf._size[1]=m;
+            surf._degree[0] = n - 1;
+            surf._degree[1] = m - 1;
+            surf._size[0] = n;
+            surf._size[1] = m;
             surf.generate_knots_u();
             surf.generate_knots_v();
             surf._update_interval();
-
-
-
-
         }
-        void computePartialDerivative(SurfaceParameter variable,Monomial2D& result) {
+
+        void computePartialDerivative(SurfaceParameter variable, Monomial2D &result) {
             Tensor2D<vec3> coeffs;
-            compute_partial_derivative(coefficients,variable,coeffs);
+            compute_partial_derivative(coefficients, variable, coeffs);
             result.set(coeffs);
-
         }
-        void computeNormal(Monomial2D& result) {
-            Tensor2D<vec3> coeffs_u,coeffs_v,coeffs;
-            compute_partial_derivative(coefficients,U,coeffs_u);
-            compute_partial_derivative(coefficients,V,coeffs_v);
-            cross(coeffs_u,coeffs_v,coeffs);
+
+        void computeNormal(Monomial2D &result) {
+            Tensor2D<vec3> coeffs_u, coeffs_v, coeffs;
+            compute_partial_derivative(coefficients, U, coeffs_u);
+            compute_partial_derivative(coefficients, V, coeffs_v);
+            cross(coeffs_u, coeffs_v, coeffs);
             result.set(coeffs);
-
         }
-        Monomial2D& operator=(const Monomial2D& other) {
+
+        Monomial2D &operator=(const Monomial2D &other) {
             if (this == &other) return *this; // Handle self-assignment
-            n=other.n,m=other.m;
-            coefficients=other.coefficients;
+            n = other.n, m = other.m;
+            coefficients = other.coefficients;
             return *this;
         }
     };
 
 
-inline Tensor3D bezier_to_monomial(const Tensor3D& control_points) {
-    size_t n = control_points.size();
-    size_t m = control_points[0].getRows();
-    size_t l = control_points[0].getCols();
+    inline Tensor3D bezier_to_monomial(const Tensor3D &control_points) {
+        size_t n = control_points.size();
+        size_t m = control_points[0].getRows();
+        size_t l = control_points[0].getCols();
 
-    Matrix Mu = bpmat(n - 1);
-    Matrix Mv = bpmat(m - 1);
+        Matrix Mu = bpmat(n - 1);
+        Matrix Mv = bpmat(m - 1);
 
-    Tensor3D monomial_coeffs(l, Matrix(n, m));
-    for (size_t d = 0; d < l; ++d) {
-        Matrix cp_slice(n, m);
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = 0; j < m; ++j) {
-                cp_slice(i, j) = control_points[i](j, d);
+        Tensor3D monomial_coeffs(l, Matrix(n, m));
+        for (size_t d = 0; d < l; ++d) {
+            Matrix cp_slice(n, m);
+            for (size_t i = 0; i < n; ++i) {
+                for (size_t j = 0; j < m; ++j) {
+                    cp_slice(i, j) = control_points[i](j, d);
+                }
             }
+            monomial_coeffs[d] = Mu * cp_slice * Mv.transpose();
         }
-        monomial_coeffs[d] = Mu * cp_slice * Mv.transpose();
+
+        return monomial_coeffs;
     }
 
-    return monomial_coeffs;
-}
+    inline Tensor3D monomial_to_bezier(const Tensor3D &monomial_coeffs) {
+        size_t n = monomial_coeffs[0].getRows();
+        size_t m = monomial_coeffs[0].getCols();
+        size_t l = monomial_coeffs.size();
 
-inline Tensor3D monomial_to_bezier(const Tensor3D& monomial_coeffs) {
-    size_t n = monomial_coeffs[0].getRows();
-    size_t m = monomial_coeffs[0].getCols();
-    size_t l = monomial_coeffs.size();
+        Matrix Mu_inv = bpmat(n - 1).inverse();
+        Matrix Mv_inv = bpmat(m - 1).inverse();
 
-    Matrix Mu_inv = bpmat(n - 1).inverse();
-    Matrix Mv_inv = bpmat(m - 1).inverse();
-
-    Tensor3D control_points(n, Matrix(m, l));
-    for (size_t d = 0; d < l; ++d) {
-        Matrix bez_slice = Mu_inv * monomial_coeffs[d] * Mv_inv.transpose();
-        for (size_t i = 0; i < n; ++i) {
-            for (size_t j = 0; j < m; ++j) {
-                control_points[i](j, d) = bez_slice(i, j);
+        Tensor3D control_points(n, Matrix(m, l));
+        for (size_t d = 0; d < l; ++d) {
+            Matrix bez_slice = Mu_inv * monomial_coeffs[d] * Mv_inv.transpose();
+            for (size_t i = 0; i < n; ++i) {
+                for (size_t j = 0; j < m; ++j) {
+                    control_points[i](j, d) = bez_slice(i, j);
+                }
             }
         }
+
+        return control_points;
     }
-
-    return control_points;
-
-}
-
-
 }
 #endif //MONOMIAL_H
