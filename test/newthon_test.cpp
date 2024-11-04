4f6546af7fd8781c68b06bd2f778a519b3f7e890
sth-v <sthv.developer@gmail.com>
2024-11-04 23:36:40 +0300

    Fix Newton's Method Implementation and Update Unit Tests
    1. **test/newthon_test.cpp**
    
       Refactor Newton's method and update unit tests.
    
       - Updated the included header from `newthon.h` to `newthon2.h`.
       - Changed the vector type for `initialPoint` and `result` to `cmmcore::Vector<2>`.
       - Modified lambda function to use `cmmcore::Vector<2>`.
       - Commented out unused `std::vector<double> initialPoint`.
    
    2. **cmmcore/newthon.h**
    
       Rename header for new implementation.
    
       - Renamed `newthon.h` to `newthon.h.bak` to reflect the transition to a new version.

--- test/newthon_test.cpp
+++ test/newthon_test.cpp
@@ -1,23 +1,24 @@
 //
 // Created by Andrew Astakhov on 26.10.24.
 //
-#include "cmmcore/newthon.h"
+#include "cmmcore/newthon2.h"
 #include "cmmcore/nurbs.h"
 int main() {
     std::vector<std::vector<cmmcore::vec4>> pts1={{{-25.0, -25.0, -10.0, 1.0}, {-25.0, -15.0, -5.0, 1.0}, {-25.0, -5.0, 0.0, 1.0}, {-25.0, 5.0, 0.0, 1.0}, {-25.0, 15.0, -5.0, 1.0}, {-25.0, 25.0, -10.0, 1.0}}, {{-15.0, -25.0, -8.0, 1.0}, {-15.0, -15.0, -4.0, 1.0}, {-15.0, -5.0, -4.0, 1.0}, {-15.0, 5.0, -4.0, 1.0}, {-15.0, 15.0, -4.0, 1.0}, {-15.0, 25.0, -8.0, 1.0}}, {{-5.0, -25.0, -5.0, 1.0}, {-5.0, -15.0, -3.0, 1.0}, {-5.0, -5.0, -8.0, 1.0}, {-5.0, 5.0, -8.0, 1.0}, {-5.0, 15.0, -3.0, 1.0}, {-5.0, 25.0, -5.0, 1.0}}, {{5.0, -25.0, -3.0, 1.0}, {5.0, -15.0, -2.0, 1.0}, {5.0, -5.0, -8.0, 1.0}, {5.0, 5.0, -8.0, 1.0}, {5.0, 15.0, -2.0, 1.0}, {5.0, 25.0, -3.0, 1.0}}, {{15.0, -25.0, -8.0, 1.0}, {15.0, -15.0, -4.0, 1.0}, {15.0, -5.0, -4.0, 1.0}, {15.0, 5.0, -4.0, 1.0}, {15.0, 15.0, -4.0, 1.0}, {15.0, 25.0, -8.0, 1.0}}, {{25.0, -25.0, -10.0, 1.0}, {25.0, -15.0, -5.0, 1.0}, {25.0, -5.0, 2.0, 1.0}, {25.0, 5.0, 2.0, 1.0}, {25.0, 15.0, -5.0, 1.0}, {25.0, 25.0, -10.0, 1.0}}};
     std::array<int,2> deg1={3,3};
     cmmcore::NURBSSurface ns1(pts1,deg1);
     auto pt=cmmcore::vec3(0.,1.,5);
-    auto function = [&pt,&ns1](const std::vector<double>& v) {
+    auto function = [&pt,&ns1](const cmmcore::Vector<2>& v)->double {
         cmmcore::vec3 vv;
         ns1.evaluate(v[0],v[1],vv);
 
         return (pt-vv).sqLength();
     };
-    std::vector<double> initialPoint = {1.0, 2.0};
+    //std::vector<double> initialPoint = {1.0, 2.0};
+    cmmcore::Vector<2> initialPoint = {1.0, 2.0};
     auto timer= cmmcore::Timer();
     timer.start();
-    std::vector<double> result = cmmcore::newtonsMethod(function, initialPoint);
+    cmmcore::Vector<2> result = cmmcore::newtonsMethod<2>(function, initialPoint);
     timer.stop();
     timer.print("newthon at: ");//232000 215875
     std::cout << "Root found at: (" << result[0] << ", " << result[1] << ")" << std::endl;
