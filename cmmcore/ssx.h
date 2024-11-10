6fb8f512fe3f369e455a723c521669813b5111b0
sth-v <sthv.developer@gmail.com>
2024-11-10 07:53:23 +0300

    Extra boundary_intersection header in csx_test
    1. test/csx_test.cpp
    Include boundary_intersection header and modify surface and curve.
    
    This commit adds the `boundary_intersection` header file and updates the data for `NURBSSurface` and `NURBSCurve` instances to use new values. These changes are likely intended to support or test new features or corrections related to boundary intersection functionalities in `csx_test.cpp`.

--- cmmcore/ssx.h
+++ cmmcore/ssx.h
@@ -7,6 +7,7 @@
 
 #include "cmmcore/nurbs.h"
 #include "cmmcore/gauss_map.h"
+#include "cmmcore/boundary_intersection.h"
 namespace cmmcore {
     static int counter=0;
     using UnorderedSetPairDouble = std::unordered_set<
@@ -23,10 +24,10 @@ namespace cmmcore {
         }
     };
     struct PatchIntersection {
-        NURBSSurface s1{};
-        NURBSSurface s2{};
+        NURBSSurface s1,s2;
+        IntersectionPoint start,end;
         PatchIntersection()=default;
-        PatchIntersection(NURBSSurface& _s1, NURBSSurface& _s2): s1(_s1), s2(_s2)
+        PatchIntersection(const NURBSSurface& _s1, const NURBSSurface& _s2,const IntersectionPoint& a,const IntersectionPoint& b ): s1(_s1), s2(_s2),start(a),end(b)
         {
 
         }
@@ -35,22 +36,54 @@ namespace cmmcore {
     };
     struct Intersection {
 
-        std::vector<PatchIntersection> patches;
-        std::vector<PointIntersection> points;
+        std::vector<PatchIntersection> patches{};
+
+        std::vector<PointIntersection> points{};
         Intersection()=default;
     };
+    inline void processSimpleSurfaceIntersection(const NURBSSurface& surface1,  const NURBSSurface& surface2, const double tol,Intersection& intersection)
+    {
+        std::vector<IntersectionPoint> ixs;
+        //printf("OOO\n");
+        find_boundary_intersections(surface1,surface2,ixs, tol);
+        if (ixs.size()>=2)
+        {
 
-    inline void detectIntersections(NURBSSurface& surface1, NURBSSurface& surface2, NURBSSurface&  gaussMap1,
-                                    NURBSSurface&  gaussMap2, double tol, Intersection& intersection, int depth=0)
+            intersection.patches.emplace_back(surface1,surface2, ixs[0], ixs[1]);
+        }
+        else
+        {
+             printf("\nError in processSimpleSurfaceIntersection %lu\n", ixs.size());
+            printf("[[");
+            for (auto& p :surface1.control_points_flat3d())
+            {
+
+                printf("[%f,%f,%f],", p.x,p.y,p.z);
+            }
+            printf("],\n");
+            printf("[");
+            for (auto& p :surface2.control_points_flat3d())
+            {
+
+                printf("[%f,%f,%f],", p.x,p.y,p.z);
+            }
+            printf("]]\n---\n");
+        }
+
+    }
+    inline void detectIntersections(  NURBSSurface& surface1, NURBSSurface& surface2,  NURBSSurface&  gaussMap1,
+                                     NURBSSurface&  gaussMap2, const double tol, Intersection& intersection, int depth=0)
     {
          counter++;
 
-        auto& bb1=surface1.bbox();
-        auto& bb2=surface2.bbox();
+        const auto& bb1=surface1.bbox();
+        const auto& bb2=surface2.bbox();
 
         if (!(bb1.intersects(bb2))) {
 #ifdef CMMCORE_DEBUG
-            printf("no intersection. depth: %d\n", depth);
+            printf("no intersection bb . depth: %d\n", depth);
+            printf("[[%f,%f,%f],[%f,%f,%f]]\n", bb1.min.x, bb1.min.y, bb1.min.z,bb1.max.x, bb1.max.y, bb1.max.z);
+            printf("[[%f,%f,%f],[%f,%f,%f]]\n", bb2.min.x, bb2.min.y, bb2.min.z,bb2.max.x, bb2.max.y, bb2.max.z);
 #endif
 
             return;
@@ -103,8 +136,7 @@ namespace cmmcore {
             printf(("["+format_vec3vec( vv)+"],["+format_vec3vec(  vv1)+"]\n\n").c_str(), depth);
             printf("gauss maps are separable. depth: %d\n", depth);
 #endif
-            intersection.patches.emplace_back(surface1,surface2);
-
+            processSimpleSurfaceIntersection(surface1,surface2,tol,intersection);
 
 
             return ;
@@ -119,6 +151,7 @@ namespace cmmcore {
         gaussMap1.subdivide(  g1[0],g1[1],g1[2],g1[3]);
         gaussMap2.subdivide(  g2[0],g2[1],g2[2],g2[3]);
 
+
         for (int i=0;i<4;i++) {
             for (int j=0;j<4;j++) {
 
@@ -128,5 +161,117 @@ namespace cmmcore {
 
 
     }
+
+
+    inline void find_start_points( NURBSSurface& surface1, NURBSSurface& surface2,const double tol,  std::vector<IntersectionPoint> &intersectionPoints )
+    {
+
+        std::vector<cmmcore::NURBSSurface> surfs;
+        std::vector<cmmcore::NURBSSurface> surfs2;
+
+
+        cmmcore::decompose(surface1,surfs);
+        cmmcore::decompose(surface2,surfs2);
+
+        std::vector<cmmcore::NURBSSurface> g1;
+        std::vector<cmmcore::NURBSSurface> g2;
+
+
+
+        for (size_t i=0;i<surfs.size();i++)
+        {
+            //surfs[i].bbox();
+            g1.push_back(cmmcore::gaussMap(surfs[i]));
+            //g1[i].bbox();
+        }
+        for (size_t i=0;i<surfs2.size();i++)
+        {
+            //surfs2[i].bbox();
+            g2.push_back(cmmcore::gaussMap(surfs2[i]));
+            //g2[i].bbox();
+        }
+
+        for (size_t i=0;i<surfs.size();i++)
+        {
+
+            for (size_t j=0;j<surfs2.size();j++)
+            {
+                cmmcore::Intersection intersection;
+
+                cmmcore::detectIntersections(surfs[i],
+                    surfs2[j],
+                    g1[i],
+                    g2[j],
+                    tol,
+                    intersection);
+                //intersectionPoints.reserve(intersectionPoints.size()+intersection.patches.size()*2);
+                if (intersection.patches.empty())
+                {
+                    continue;
+                }
+
+                for (auto & patche : intersection.patches)
+                {
+
+                    processSimpleSurfaceIntersection(patche.s1, patche.s2, tol, intersection);
+
+                    //find_boundary_intersections(patche.s1, patche.s2, intersectionPoints,tol);
+
+                }
+
+
+
+            }
+        }
+    }
+    inline void detectAllInts(const NURBSSurface& ns1,const NURBSSurface& ns2, const double tol,cmmcore::Intersection& intersection) {
+
+    std::vector<cmmcore::NURBSSurface> surfs;
+    std::vector<cmmcore::NURBSSurface> surfs2;
+
+
+    cmmcore::decompose(ns1,surfs);
+    cmmcore::decompose(ns2,surfs2);
+
+    std::vector<cmmcore::NURBSSurface> g1;
+    std::vector<cmmcore::NURBSSurface> g2;
+
+
+
+
+        for (size_t i=0;i<surfs.size();i++)
+        {
+            //surfs[i].bbox();
+            g1.push_back(cmmcore::gaussMap(surfs[i]));
+            //g1[i].bbox();
+        }
+        for (size_t i=0;i<surfs2.size();i++)
+        {
+            ///surfs2[i].bbox();
+            g2.push_back(cmmcore::gaussMap(surfs2[i]));
+            //g2[i].bbox();
+        }
+
+    for (size_t i=0;i<surfs.size();i++)
+    {
+
+        for (size_t j=0;j<surfs2.size();j++)
+        {
+
+                    cmmcore::detectIntersections(surfs[i],
+                        surfs2[j],
+                        g1[i],
+                        g2[j],
+                        tol,
+                        intersection);
+
+
+        }
+    }
+
+
+
+
+}
 }
 #endif //SSX_H
